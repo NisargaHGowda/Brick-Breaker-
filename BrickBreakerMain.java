d6c9a6a312f6e7e7945cb2f248b3fef9a9cfa539
Nisarga.gowda <158475422+NisargaHGowda@users.noreply.github.com>
2024-12-05 14:12:03 +0530

    Update BrickBreakerMain.java

--- BrickBreakerMain.java
+++ BrickBreakerMain.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PingPong;
 
 import javax.swing.JFrame;
 
-/**
- *
- * @author Tunkis
- */
+
 public class BrickBreaker {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
        JFrame fr=new JFrame();
        Play play=new Play();
