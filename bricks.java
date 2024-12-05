da31a9165fa1aebfef10aeb5e5e4f5a80f73db2f
Nisarga.gowda <158475422+NisargaHGowda@users.noreply.github.com>
2024-12-05 14:13:11 +0530

    Update bricks.java

--- bricks.java
+++ bricks.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PingPong;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-/**
- *
- * @author Tunkis
- */
+
 public class bricks {
 
     public int brickwidth;
